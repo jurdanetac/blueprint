43dddda383d2b942a2bd19ef5cdc885e145554bf
Juan Urdaneta <jurdanetac10@gmail.com>
2026-05-22 10:00:33 -0400

    Install main app and render template

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blueprint.apps.BlueprintConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
