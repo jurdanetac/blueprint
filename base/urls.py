2089518d8f3e147871b1d5e058b2223e45b0380d
Juan Urdaneta <jurdanetac10@gmail.com>
2026-05-25 17:15:14 -0400

    Allow TODO deletion

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # GET: Renders homepage
     path("", views.index, name="index"),
+    # POST: Deletes a todo item
+    path("<int:todo_id>/delete/", views.delete_todo, name="delete-todo"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from http import HTTPStatus
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+# from django.views.decorators.csrf import csrf_exempt
 
 from base.forms import TodoForm
 from base.models import Todo
 
 # Create your views here.
+
+
+def delete_todo(request, todo_id):
+    if request.method != "POST":
+        return HttpResponse(status=HTTPStatus.METHOD_NOT_ALLOWED)
+
+    # Safely find the todo item or return a 404 if it doesn't exist
+    todo = get_object_or_404(Todo, id=todo_id)
+
+    # Delete the todo from the database
+    todo.delete()
+
+    return HttpResponse("", status=HTTPStatus.OK)
 
 
 def index(request):
