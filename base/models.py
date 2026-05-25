8be43f6e4ccd79a33276107980a01a29ae158915
Juan Urdaneta <jurdanetac10@gmail.com>
2026-05-25 17:29:08 -0400

    Add timestamped model

--- base/models.py
+++ base/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+# Source - https://stackoverflow.com/a/8016679
+class TimeStampedModel(models.Model):
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
 
-class Todo(models.Model):
+     class Meta:
+         abstract = True
+
+
+class Todo(TimeStampedModel):
     # short length, use description for extended notes
     task = models.CharField(max_length=30)
     # description = models.CharField(max_length=255)
