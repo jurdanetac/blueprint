62de2fc6e7793ccdf5aed2c9689e9a1bef02a225
Juan Urdaneta <jurdanetac10@gmail.com>
2026-06-04 23:11:45 +0000

    Add styles to the TODO form

--- base/forms.py
+++ base/forms.py
@@ -11,3 +11,14 @@ class TodoForm(forms.ModelForm):
             # This overrides the default widget for 'due_on' with the custom HTML5 date picker widget
             "due_on": forms.DateInput(attrs={"type": "date"}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Loop through all fields and apply a standard styling class
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update(
+                {
+                    "class": "p-1 border rounded-md bg-slate-50 text-slate-900 focus:ring-2 focus:ring-blue-400"
+                }
+            )
